fc02cea5187204eab3940f55f3c3e51457008cb0
asconaga <asconaga@gmail.com>
2025-07-25 00:42:34 +0100

    using icon for header settings

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,6 +1,7 @@
+import { Ionicons } from '@expo/vector-icons';
 import { Stack, useRouter } from "expo-router";
 import { useEffect } from "react";
-import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { Platform, StyleSheet, TouchableOpacity } from "react-native";
 import { initializeStatus } from "./services/statusDB";
 
 // change the following to not show the header for index
@@ -27,8 +28,12 @@ export default function RootLayout() {
         options={{
           title: 'MADS Test',
           headerRight: () => (
-            <TouchableOpacity onPress={() => router.push('/optionsScreen')} style={{ marginLeft: 10 }}>
-              <Text style={styles.text}>≡≡</Text>
+            <TouchableOpacity
+              onPress={() =>
+                router.push('/optionsScreen')}
+              hitSlop={{ top: 30, bottom: 30, left: 30, right: 30 }}
+              style={styles.button}>
+              <Ionicons name="settings-outline" size={22} color="white" />
             </TouchableOpacity>
           )
         }}
@@ -51,5 +56,14 @@ const styles = StyleSheet.create({
     paddingLeft: 2,
     paddingRight: 2,
   },
+  button: {
+    ...Platform.select({
+      web: {
+        marginRight: 12,
+      },
+      default: {
+        marginRight: -12
+      },
+    }),
+  },
 });
-
